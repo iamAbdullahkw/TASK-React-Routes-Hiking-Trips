d8bf819250f0f189295054558d74624677d29a7d
Abdullah Fahad <iamabdullahkw@gmail.com>
2023-06-04 22:49:47 +0300

    slug instead ID

--- src/components/TripDetail.js
+++ src/components/TripDetail.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import tripsData from '../tripsData';
-
+import React from "react";
+import tripsData from "../tripsData";
+import { useParams, Navigate } from "react-router-dom";
 function TripDetail() {
-  const trip = tripsData[0];
+  const { tripsslug } = useParams();
+
+  const trip = tripsData.find((trip) => trip.slug === tripsslug);
+  if (!trip) {
+    return <Navigate to="/" />;
+  }
   return (
     <div className="modal-dialog modal-xl">
       <div className="modal-content">
